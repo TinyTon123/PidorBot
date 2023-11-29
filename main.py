058ac4e532812b7cea7ffd72523c5c4a552f6ab7
TinyTon123 <konanton_2004@bk.ru>
2023-11-29 03:10:02 +0300

    Wednesday regular message added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import asyncio
 import logging
+import datetime
 
 from aiogram import Bot, Dispatcher
 
@@ -20,8 +21,30 @@ async def main() -> None:
     dp.include_router(re_handlers.router)
     dp.include_router(chat_members_handlers.router)
     bot: Bot = Bot(bot_token, parse_mode="HTML")
+
+    async def send_message_on_specific_day():
+        # Определите, в какой день недели нужно отправлять сообщение (0 - понедельник, 6 - воскресенье)
+        target_day = 2
+        target_hour = 7
+
+        while True:
+            # Получаем текущий день недели
+            current_day = datetime.datetime.today().weekday()
+            current_hour = datetime.datetime.today().hour
+
+            if (current_day == target_day) and (current_hour >= target_hour):
+                # Отправляем сообщение
+                await bot.send_message(chat_id=391639940, text="Регулярное сообщение")
+                # Ждем неделю перед отправкой следующего сообщения
+                await asyncio.sleep(60 * 30)
+            else:
+                # Если текущий день недели не является целевым, ждем 1 час и проверяем снова
+                await asyncio.sleep(60 * 60)
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(send_message_on_specific_day())
     await bot.delete_webhook(drop_pending_updates=True)
-    await bot.send_message(391639940, f"{replies['startup']}")
+    await bot.send_message(chat_id=391639940, text=f"{replies['startup']}")
     await dp.start_polling(bot)
 
 
